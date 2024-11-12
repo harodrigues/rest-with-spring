e8e304ffc8aed98ba4b31f57d7d02b0b7829a401
Helcio <helciorgs@outlook.com>
2024-11-12 13:52:02 -0300

    Primeiros passos

---
+++ configuracao/rest-with-spring/src/main/java/br/com/curso/Saudacao.java
@@ -0,0 +1,20 @@
+package br.com.curso;
+
+public class Saudacao {
+	private final long id;
+	private final String content;
+
+	public Saudacao(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
